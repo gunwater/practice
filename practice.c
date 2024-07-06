04dd51544ecbfad4d6b9064b0c8ff05489c8c8dd
gunsu <kirjstn@gmail.com>
2024-07-07 02:19:46 +0900

    2:19

--- practice.c
+++ practice.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello,dddddddddddddddddddd World!\n");
 
-<<<<<<< HEAD
-    while(1) {
-        printf("Hello, World!\n");
-    }
     return 0;
-=======
-    // 리더 부분 추가
-    // 리더 부분 추가2
+
+
     
->>>>>>> 96dac25b727c597bc5b8697f9fb37edcbc3c0207
 }
