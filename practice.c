651dc0673b3702fa112716e89ba84c83b7e42f72
gunsu <kirjstn@gmail.com>
2024-07-07 02:10:13 +0900

    while add

--- practice.c
+++ practice.c
@@ -3,12 +3,8 @@
 int main() {
     printf("Hello, World!\n");
 
-    for(int i = 0; i < 10; i++) {
+    while(1) {
         printf("Hello, World!\n");
     }
-
     return 0;
-
-    // 리더 부분 추가
-    
 }
